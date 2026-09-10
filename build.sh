4569b686057d8d21bda016dc305ef719cff38164
TheTechLeadKE <thetechleadke@gmail.com>
2026-09-10 13:13:51 +0300

    added down,left and right controls

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 cp ../app .
 
-./app --profile c4 --target x86_64-linux-gnuc -i src/main.c4 -o main -l raylib
+./app --profile c4 --target x86_64-linux-gnuc -i src/main.c4 -o main -l m -l raylib
 
 if [ $1 -eq 1 ];
 then
